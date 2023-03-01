ce00ee42f78aac8a6b15d234b6c05e429e806b15
xiguan-wuge <1584077483@qq.com>
2023-03-02 05:53:54 +0800

    feat: 前后端添加gzip压缩

--- server/staticResourceServer/server.js
+++ server/staticResourceServer/server.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const path = require('path')
 const fs = require('fs')
+const zlib = require('zlib')
 
 const typeList = {
   '.png': 'image/png',
@@ -14,7 +15,6 @@ const server = http.createServer((req, res) => {
   console.log('url', req.url);
   const url = req.url === '/' ? '/index.html' : req.url
   const filePath = path.join(__dirname, 'static', url)
-  console.log('filePaht',filePath);
   fs.readFile(filePath, (err, data) => {
     if(err) {
       console.log('readFile-err', err)
@@ -22,12 +22,17 @@ const server = http.createServer((req, res) => {
       res.end('not found')
     } else {
       const ext = path.extname(filePath)
-      console.log('ext', ext);
       if(typeList[ext]) {
         res.setHeader('content-type', typeList[ext])
         if(url.indexOf('index.html') === -1) {
           // 静态资源添加缓存,默认365天（参考dev环境中，静态资源的时间）
           res.setHeader('cache-control', 'max-age=31536000')
+          const acceptEncodingValue = req.headers['accept-encoding'] || 'gzip'
+          if(acceptEncodingValue.includes('gzip')) {
+            res.setHeader('content-encoding', 'gzip')
+            // 使用同步API直接压缩
+            data = zlib.gzipSync(Buffer.from(data))
+          }
         }
       }
       res.end(data)
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { visualizer } from 'rollup-plugin-visualizer';
 import {resolve} from 'path'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,9 @@ export default defineConfig({
       open:true,
       gzipSize: true,
       brotliSize: true
+    }),
+    viteCompression({
+      threshold: 1024000, // 对大于1M的文件开启gzip压缩
     })
   ],
   resolve: {
