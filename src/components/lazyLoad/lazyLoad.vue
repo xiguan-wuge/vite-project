a033f55a70aeeb34bdaf26479edfa04b7c678ccd
xiguan-wuge <1584077483@qq.com>
2023-03-27 09:49:50 +0800

    feat: lazy-load组件

---
+++ src/components/lazyLoad/lazyLoad.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="lazy-load">
+    <!-- 知乎评论占位图 -->
+    <img :src="tempImg" alt="" class="img-item" :data-src="tempImg">
+    
+    <img v-for="item in list" :src="defaultImg" :data-src="item" alt="" class="img-item" >
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {ref, onMounted} from 'vue'
+import {list, defaultImg} from './list.js'
+import tempImg from '@/assets/tempImg.svg'
+onMounted(() => {
+  // const root = document.querySelector('.lazy-load')
+  const root = document.querySelector('body')
+  console.log('root', root);
+  const options = {
+    root: root,
+    threshold: [0], // 交汇处
+    rootMargin: '0px', // 对视口进行收缩和扩张
+  }
+  const lazyIntersection = new IntersectionObserver(entries => {
+    // entries: 监听的节点数组对象
+    entries.forEach((item, index) => {
+      // console.log('item', item);
+      // if(item.isIntersecting) {
+      if(item.intersectionRatio > 0){
+        // 进入可视区域
+        item.target.src = item.target.getAttribute('data-src')
+        // 停止进行观察
+        lazyIntersection.unobserve(item.target)
+      }
+    })
+  }, options)
+
+  // const data = Array.from(root.children)
+  const data = Array.from(document.querySelectorAll('img'))
+
+  console.log('data', data);
+
+  data.forEach(item => {
+    lazyIntersection.observe(item)
+  })
+  
+
+})
+
+
+</script>
+
+<style lang="less" scoped>
+.lazy-load {
+  width: 300px;
+  // height: 1000px;
+  background-color: pink;
+  overflow-y: scroll;
+}
+.img-item {
+  width: 100%;
+  min-height: 100px;
+  height: auto;
+}
+</style>
---
+++ src/components/lazyLoad/list.js
@@ -0,0 +1,12 @@
+export const list = [
+  'https://www.ssfiction.com/wp-content/uploads/2020/08/20200805_5f2b1669e9a24.jpg',
+  'https://img2.woyaogexing.com/2023/03/12/d3fb4d961ea7f01e70b345f88e919050.jpg',
+  'https://img2.woyaogexing.com/2023/03/12/de387d023dda4e6d8abb740ec07fbca8.jpg',
+  'https://img2.woyaogexing.com/2023/03/12/23f6047acceed58fd206c2032efb3657.jpg',
+  'https://img2.woyaogexing.com/2023/03/12/4740477763c731f8c026411befe93e67.jpg',
+  'https://img2.woyaogexing.com/2023/03/12/848ac90d28a208434e899dc5b781b6a4.jpg',
+  'https://img2.woyaogexing.com/2023/03/12/b366316090f8045b397dede675b0e3f9.jpg',
+  'https://img2.woyaogexing.com/2023/03/12/9b42f19cbe32012961d9d50ce7f30008.jpg',
+]
+
+export const defaultImg = 'https://trademark.zbjimg.com/pattern-prod/2018/image_16/29717963.jpg'
